d0cd7d52ab4d00c8b49f7927eeb9ef367b30e980
Guy Peled <guy.p@taboola.com>
2019-08-04 17:59:03 +0300

    graphql

--- built_graphql_client_generator/lib/src/generator.dart
+++ built_graphql_client_generator/lib/src/generator.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:build/src/builder/build_step.dart';
+import 'package:petitparser_extras/petitparser_extras.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'package:built_graphql_client/built_graphql_client.dart';
@@ -9,6 +10,20 @@ class GraphqlGenerator extends GeneratorForAnnotation<GraphqlQuery> {
   @override
   FutureOr<String> generateForAnnotatedElement(
       Element element, ConstantReader annotation, BuildStep buildStep) {
+
+
     return "// Hey! Annotation found!";
+  }
+
+  String generateCommand(String className, String query, AstNode schema) {
+
+    GraphQLParser parser = GraphQLParser();
+
+    return generateCommandCode(parser.parseToAstWithSchemaAst(query, schema));
+  }
+
+  String generateCommandCode(AstNode query) {
+    // query.children.whereType<TypeDefinition>();
+    return "d";
   }
 }
